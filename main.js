c22382ed56cbf686c9951b4295e5b762ff78d93e
Chris Amling <camling@mppl.org>
2016-10-12 13:26:55 -0500

    initial commit. Working basic version of todo web app.  Uses JS localstorge to keep todo state in browser.

---
+++ main.js
@@ -0,0 +1,154 @@
+function todoProgram()
+{
+  
+  this.todos = [];
+  this.list = document.getElementById("list");
+  this.inputData = document.getElementById("inputData");
+  this.inputButton = document.getElementById("inputButton");
+  this.form = document.getElementById("form");
+  var _this = this;
+
+
+  
+  this.init = function()
+  {
+
+
+  	// Check LocalStorage
+  	this.checkLocalStorage();
+
+     // Add EventListeners
+    inputButton.addEventListener("click",function(e)
+    {
+      _this.clickedFunc(_this.inputData.value);
+    });
+
+    inputButton.addEventListener("touch",function(e)
+    {
+      _this.clickedFunc(_this.inputData.value);
+    });
+
+    form.addEventListener("submit",function(e)
+    {
+      _this.clickedFunc(_this.inputData.value);
+    });
+
+  
+  }
+
+  this.checkLocalStorage = function()
+  {
+  	if (typeof(Storage) !== "undefined") 
+  	{
+  		console.log("You have localStorage");
+  		
+
+  		if(localStorage.getItem("toDoArray").length != 0)
+  		{
+			this.todos = localStorage.getItem("toDoArray").split(",");
+			this.createList();
+  		}
+  		
+  	}
+  }
+
+
+  this.updateLocalStorage = function()
+  {
+
+
+  	if (typeof(Storage) !== "undefined") 
+  	{
+  		localStorage.setItem("toDoArray", this.todos.toString());
+	}
+
+  }
+ 
+  this.checkInputData = function()
+  {
+    if(this.inputData.value.length === 0)
+      {
+        return false;
+      }
+    else
+      {
+        return true;
+      }
+  }
+  
+  this.clearInputData = function()
+  {
+    inputData.value = "";
+  }
+  
+  
+  this.clearList = function()
+  {
+    this.list.innerHTML = "";
+  }
+  
+  this.createList = function()
+  {
+    
+    function triggerRemove()
+    {
+       _this.remove(this.hold);
+       _this.updateLocalStorage();
+    }
+    
+    var listLength = this.todos.length;
+    for(var i = 0; i < listLength; i++)
+      {
+        var li = document.createElement("li");
+        var text = document.createTextNode(this.todos[i]);
+        li.hold = i;
+        li.className = "listItem";
+        li.addEventListener("click",triggerRemove);
+        li.appendChild(text);
+        list.appendChild(li);
+      }
+  }
+  
+  
+      
+    this.clickedFunc = function(x)
+    {
+      if(this.checkInputData())
+        {
+          this.add(x);
+          this.clearInputData();
+          this.clearList();
+          this.createList();
+          this.updateLocalStorage();
+
+
+        }
+     
+    }
+  
+    this.add = function(x)
+    {
+      this.todos.push(x);      
+    }
+  
+    this.remove = function(pos)
+    {
+      this.todos.splice(pos, 1);
+      this.clearList();
+      this.createList();
+    }
+  
+    this.getAll = function()
+    {
+      return this.todos;
+    }
+    this.showAll = function()
+    {
+    console.log(this.getAll());
+    }
+
+    this.init();
+  
+}
+  
+var todoObj = new todoProgram();
